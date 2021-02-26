3ff86e998d489512d317a915b751eeca4c16247c
MarcG <marc.gilbert@sky.uk>
2021-02-26 13:16:50 +0000

    creates empty activity for paris events list

---
+++ app/src/main/java/com/example/ui/parisEvents/ParisEventListActivity.kt
@@ -0,0 +1,12 @@
+package com.example.ui.parisEvents
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.baseandroidapp.R
+
+class ParisEventListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_paris_event_list)
+    }
+}
